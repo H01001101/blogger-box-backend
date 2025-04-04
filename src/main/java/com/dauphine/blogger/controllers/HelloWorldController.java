ae1056012809796c683e472af9d0bd7165c9f6af
Martin Hamelle <martin@MacBook-Air-de-Martin.local>
2025-04-04 11:25:29 +0200

    add documentation via swagger 2

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,4 +1,36 @@
 package com.dauphine.blogger.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@Tag(
+        name = "Hello world API",
+        description = "My First hello world endpoints"
+)
 public class HelloWorldController {
+
+    @GetMapping("hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("hello")
+    public String helloByName(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("hello/{name}")
+    @Operation(
+        summary = "Hello by name endpoint",
+            description = "Returns 'Hello {name}', by path variable"
+    )
+    public String hello(@Parameter(description = "Name to greet") @PathVariable String name) {
+        return "Hello " + name;
+    }
 }
